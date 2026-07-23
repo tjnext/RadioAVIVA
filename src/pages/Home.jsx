7c8013881d265560611a52d338c301082dca60ad
next <116184333+tjnext@users.noreply.github.com>
2026-07-23 12:04:04 -0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,6 @@ import Header from '../components/Header';
 import Hero from '../components/Hero';
 import Programacao from '../components/Programacao';
 import Cultos from '../components/Cultos';
-import Eventos from '../components/Eventos';
 import PedidoOracao from '../components/PedidoOracao';
 import RedesSociais from '../components/RedesSociais';
 import Footer from '../components/Footer';
